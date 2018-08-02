1c8dc8af9b324bb830ad99ef848ec773d42f2279
dnackat <dileepbn@gmail.com>
2018-08-03 02:16:53 +0530

    added function to extract tuple data

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -320,3 +320,18 @@ def polysum(n, s):
     perimeter_sq = (n * s)**2
     # Return their sum
     return round(area + perimeter_sq, 2)
+
+#%% Tuples
+def get_data(aTuple):
+    nums = ()
+    words = ()
+    
+    for t in aTuple:
+        nums = nums + (t[0],)
+        if t[1] not in words:
+            words = words + (t[1],)
+    min_nums = min(nums)
+    max_nums = max(nums)
+    unique_words = len(words)
+    
+    return (min_nums, max_nums, unique_words)
