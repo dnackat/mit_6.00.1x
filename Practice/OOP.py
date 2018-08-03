a6829b3c69fe61ef3a30584a604cb976bb5c336d
dnackat <dileepbn@gmail.com>
2018-08-04 00:46:13 +0530

    function to analyze song lyrics

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -361,3 +361,43 @@ def applyToEach(L, f):
 def applyFuns(L, x):
     for f in L:
         print(f(x))
+        
+#%% Dicts: Analyze song lyrics
+def lyrics_to_frequencies(lyrics):
+    """ Frequency of words. 'lyrics' is a list of strings. """
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+            
+    return myDict
+
+def most_common_words(freqs):
+    """ Most common words. 'freqs' is a dictionary. """
+    values = freqs.values()
+    best = max(values)
+    words = []
+    
+    for k in freqs:
+        if freqs[k] == best:
+            words.append(k)
+            
+    return (words, best)
+
+def words_often(freqs, minTimes):
+    """ Words in freqs appearing atleast 'minTimes' number 
+        of times. 'freqs' is a dictionary."""
+    result = []
+    done = False
+    while not done:
+        temp = most_common_words(freqs)
+        if temp[1] >= minTimes:
+            result.append(temp)
+            for w in temp[0]:
+                del(freqs[w])
+        else:
+            done = True
+            
+    return result
