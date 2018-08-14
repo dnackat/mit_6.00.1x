5bd01c193ea52ad222bd3e97e4aff4bbeb49539c
dnackat <dileepbn@gmail.com>
2018-08-15 01:02:23 +0530

    added a few more OOP and generator funcs

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -705,6 +705,26 @@ class Rabbit(Animal):
         self.parent2 = parent2
         self.rid = Rabbit.tag
         Rabbit.tag += 1
+        
+    def get_rid(self):
+        return str(self.rid).zfill(3)
+    
+    def get_parent1(self):
+        return self.parent1
+    
+    def get_parent2(self):
+        return self.parent2
+    
+    def __add__(self, other):
+        # Returning object of same type as this class (as in, mating)
+        return Rabbit(0, self, other)   # Age = 0, parent1 = self, parent2 = other
+    
+    def __eq__(self, other):
+        parents_same = self.parent1.rid == other.parent1.rid \
+                       and self.parent2.rid == other.parent2.rid
+        parents_opposite = self.parent2.rid == other.parent1.rid \
+                       and self.parent1.rid == other.parent2.rid
+        return parents_same or parents_opposite
     
     def speak(self):
         print("meep")
@@ -798,3 +818,286 @@ spell = Accio()
 spell.execute()
 studySpell(spell)
 studySpell(Confundo())
+
+#%% OOP: An extended example
+import datetime
+
+class Person(object):
+    def __init__(self, name):
+        """create a person called name"""
+        self.name = name
+        self.birthday = None
+        self.lastName = name.split(' ')[-1]
+
+    def getLastName(self):
+        """return self's last name"""
+        return self.lastName
+        
+    def setBirthday(self,month,day,year):
+        """sets self's birthday to birthDate"""
+        self.birthday = datetime.date(year,month,day)
+
+    def getAge(self):
+        """returns self's current age in years"""
+        if self.birthday == None:
+            raise ValueError
+        return ((datetime.date.today() - self.birthday).days) // 365
+        
+    
+    def __lt__(self, other):
+        """return True if self's name is lexicographically
+           less than other's name, and False otherwise"""
+        if self.lastName == other.lastName:
+            return self.name < other.name
+        return self.lastName < other.lastName
+    
+    # other methods
+
+    def __str__(self):
+        """return self's name"""
+        return self.name
+    
+class MITPerson(Person):
+    nextIdNum = 0 # next ID number to assign
+
+    def __init__(self, name):
+        Person.__init__(self, name) # initialize Person attributes
+        # new MITPerson attribute: a unique ID number
+        self.idNum = MITPerson.nextIdNum
+        MITPerson.nextIdNum += 1
+
+    def getIdNum(self):
+        return self.idNum
+
+    # sorting MIT people uses their ID number, not name!
+    def __lt__(self, other):
+        return self.idNum < other.idNum
+    
+    def speak(self, utterance):
+        return (self.name + " says: " + utterance)
+    
+class Student(MITPerson):
+    pass
+
+class UG(Student):
+    def __init__(self, name, classYear):
+        MITPerson.__init__(self, name)
+        self.year = classYear
+
+    def getClass(self):
+        return self.year
+        
+    def speak(self, utterance):
+        return MITPerson.speak(self, " Yo bro, " + utterance)
+
+class Grad(Student):
+    pass
+
+class TransferStudent(Student):
+    pass
+
+def isStudent(obj):
+    return isinstance(obj,Student)
+
+class Professor(MITPerson):
+    def __init__(self, name, department):
+        MITPerson.__init__(self, name)
+        self.department = department
+        
+    def speak(self, utterance):
+        newUtterance = 'In course ' + self.department + ' we say '
+        return MITPerson.speak(self, newUtterance + utterance)
+        
+    def lecture(self, topic):
+        return self.speak('it is obvious that ' + topic)
+    
+# Gradebook
+class Grades(object):
+    """A mapping from students to a list of grades"""
+    def __init__(self):
+        """Create empty grade book"""
+        self.students = []  # list of Student objects
+        self.grades = {}    # maps idNum -> list of grades
+        self.isSorted = True # true if self.students is sorted
+
+    def addStudent(self, student):
+        """Assumes: student is of type Student
+           Add student to the grade book"""
+        if student in self.students:
+            raise ValueError('Duplicate student')
+        self.students.append(student)
+        self.grades[student.getIdNum()] = []
+        self.isSorted = False
+
+    def addGrade(self, student, grade):
+        """Assumes: grade is a float
+           Add grade to the list of grades for student"""
+        try:
+            self.grades[student.getIdNum()].append(grade)
+        except KeyError:
+            raise ValueError('Student not in grade book')
+
+    def getGrades(self, student):
+        """Return a list of grades for student"""
+        try:    # return copy of student's grades
+            return self.grades[student.getIdNum()][:]
+        except KeyError:
+            raise ValueError('Student not in grade book')
+
+
+    def allStudents(self):
+        """Return a list of the students in the grade book"""
+        if not self.isSorted:
+            self.students.sort()
+            self.isSorted = True
+        #return self.students[:]
+        #return copy of list of students
+        for s in self.students:
+            yield s
+
+
+
+def gradeReport(course):
+    """Assumes: course if of type grades"""
+    report = []
+    for s in course.allStudents():
+        tot = 0.0
+        numGrades = 0
+        for g in course.getGrades(s):
+            tot += g
+            numGrades += 1
+        try:
+            average = tot/numGrades
+            report.append(str(s) + '\'s mean grade is '
+                          + str(average))
+        except ZeroDivisionError:
+            report.append(str(s) + ' has no grades')
+    return '\n'.join(report)
+    
+#%% Wordgame in OOP
+import random 
+
+class Hand(object):
+    def __init__(self, n):
+        '''
+        Initialize a Hand.
+
+        n: integer, the size of the hand.
+        '''
+        assert type(n) == int
+        self.HAND_SIZE = n
+        self.VOWELS = 'aeiou'
+        self.CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+
+        # Deal a new hand
+        self.dealNewHand()
+
+    def dealNewHand(self):
+        '''
+        Deals a new hand, and sets the hand attribute to the new hand.
+        '''
+        # Set self.hand to a new, empty dictionary
+        self.hand = {}
+
+        # Build the hand
+        numVowels = self.HAND_SIZE // 3
+    
+        for i in range(numVowels):
+            x = self.VOWELS[random.randrange(0,len(self.VOWELS))]
+            self.hand[x] = self.hand.get(x, 0) + 1
+        
+        for i in range(numVowels, self.HAND_SIZE):    
+            x = self.CONSONANTS[random.randrange(0,len(self.CONSONANTS))]
+            self.hand[x] = self.hand.get(x, 0) + 1
+            
+    def setDummyHand(self, handString):
+        '''
+        Allows you to set a dummy hand. Useful for testing your implementation.
+
+        handString: A string of letters you wish to be in the hand. Length of this
+        string must be equal to self.HAND_SIZE.
+
+        This method converts sets the hand attribute to a dictionary
+        containing the letters of handString.
+        '''
+        assert len(handString) == self.HAND_SIZE, "Length of handString ({0}) must equal length of HAND_SIZE ({1})".format(len(handString), self.HAND_SIZE)
+        self.hand = {}
+        for char in handString:
+            self.hand[char] = self.hand.get(char, 0) + 1
+
+
+    def calculateLen(self):
+        '''
+        Calculate the length of the hand.
+        '''
+        ans = 0
+        for k in self.hand:
+            ans += self.hand[k]
+        return ans
+    
+    def __str__(self):
+        '''
+        Display a string representation of the hand.
+        '''
+        output = ''
+        for letter in sorted(self.hand.keys()):
+            output += letter * self.hand[letter]
+        return output
+
+    def update(self, word):
+        """
+        Does not assume that self.hand has all the letters in word.
+
+        Updates the hand: if self.hand does have all the letters to make
+        the word, modifies self.hand by using up the letters in the given word.
+
+        Returns True if the word was able to be made with the letter in
+        the hand; False otherwise.
+        
+        word: string
+        returns: Boolean (if the word was or was not made)
+        """
+        ret_hand = self.hand.copy()
+        
+        if self.calculateLen() < len(word):
+            return False
+        else:
+            for letter in word:
+                if ret_hand.get(letter, 0) < 1:
+                    return False
+                else:
+                    ret_hand[letter] -= 1
+        
+        self.hand = ret_hand
+        
+        return True
+    
+#%% Fancy Fibonacci
+def genFib():
+    fibn_1 = 1 #fib(n-1)
+    fibn_2 = 0 #fib(n-2)
+    while True:
+        # fib(n) = fib(n-1) + fib(n-2)
+        next = fibn_1 + fibn_2
+        yield next
+        fibn_2 = fibn_1
+        fibn_1 = next
+        
+#%% Prime number generator
+def genPrimes():
+    primes = [2]
+    curr = 2
+    while True:
+        next = curr
+        yield next
+        while True:
+            isPrime = True
+            curr += 1
+            for p in primes:
+                if curr % p == 0:
+                    isPrime = False
+                    break
+                
+            if isPrime:
+                primes.append(curr)
+                break
