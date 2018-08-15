011043cfc3ef1bca03bad936b02cdf241d29311b
dnackat <dileepbn@gmail.com>
2018-08-16 01:47:21 +0530

    added more funcs

--- Practice/OOP.py
+++ Practice/OOP.py
@@ -1101,3 +1101,18 @@ def genPrimes():
             if isPrime:
                 primes.append(curr)
                 break
+            
+#%% Generating subsets: exponential complexity algorithm
+def genSubsets(L):
+    if len(L) == 0:
+        return [[]] #list of empty list
+    smaller = genSubsets(L[:-1]) # all subsets without last element
+    extra = L[-1:] # create a list of just last element
+    new = []
+    for small in smaller:
+        new.append(small+extra)  # for all smaller solutions, add one with last element
+    return smaller+new  # combine those with last element and those without
+
+test = [1,2,3,4]
+
+super = genSubsets(test)
