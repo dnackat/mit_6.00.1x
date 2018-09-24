67659ea496d4bc15830ce543b9043602384b3000
dnackat <dileepbn@gmail.com>
2018-09-24 13:30:18 +0530

    some more prac

---
+++ Practice/test2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 13:25:47 2018
+
+@author: dileepn
+"""
+
+def main():
+    print("I'm in main!")
+    
+if __name__ == "__main__":
+    main()
