a24cfcad13d388890fbad6d9c2ecbee1ae72e488
dnackat <dileepbn@gmail.com>
2019-03-17 12:03:32 +0530

    added eigenvalue calc

--- Practice/funcmin.py
+++ Practice/funcmin.py
@@ -9,10 +9,14 @@ Created on Wed Mar 13 22:36:09 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-4,4,100)
-y = (1/3)*x**3 - x**2 - 3*x + 10
+x = np.linspace(0,25,1000)
+y = 1/(np.exp(x)-1)
 
 plt.plot(x,y,'r-')
 
 print("max =", max(y))
 print("min =", min(y))
+
+H = np.array([[-1,-1],[-1,-5]])
+
+print(np.linalg.eigvals(H))
