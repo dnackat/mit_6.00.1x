a55c276e2c05ead94949f9569c83f00c4367996b
dnackat <dileepbn@gmail.com>
2019-04-11 23:10:24 +0530

    created code blocks for better organization

--- Practice/funcmin.py
+++ Practice/funcmin.py
@@ -9,6 +9,7 @@ Created on Wed Mar 13 22:36:09 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%%
 x = np.linspace(1,1.2,100)
 a = 1
 n = 100
@@ -19,10 +20,12 @@ plt.plot(x,y,'r-')
 print("max =", max(y))
 print("min =", min(y))
 
+#%%
 H = np.array([[-1,-1],[-1,-5]])
 
 print(np.linalg.eigvals(H))
 
+#%%
 # p-value calculation
 n = 100
 mubar = 3.28
@@ -30,6 +33,7 @@ sigma2bar = 15.95
 
 Tn = np.sqrt(2*n/(3*sigma2bar))*(np.sqrt(sigma2bar)-mubar)
 
+#%%
 # Welsh-Satterthwaite formula calculation
 sigma1sqhat = 0.1
 sigma2sqhat = 0.2
